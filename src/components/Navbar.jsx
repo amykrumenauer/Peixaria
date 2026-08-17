241a9022c920eb14f0e5e57ea3266c03165db425
Amanda Krumenauer <krumenaueramy5@gmail.com>
2026-08-17 14:09:13 -0300

    melhorias

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Peixaria & Empório do Vale</div>
+      <div className="logo">P e i x a r i a  &  E m p ó r i o  d o  V a l e</div>
 
       <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#produtos">Produtos</a></li>
-        <li><a href="#sobre">Sobre nós</a></li>
-        <li><a href="#contato">Contato</a></li>
+        <li><a href="#home">H o m e</a></li>
+        <li><a href="#produtos">P r o d u t o s</a></li>
+        <li><a href="#sobre">S o b r e  n ó s</a></li>
+        <li><a href="#contato">C o n t a t o</a></li>
       </ul>
     </nav>
   );
