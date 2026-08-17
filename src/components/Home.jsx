2f629630d5a4059c76b6e63df1e68fe51716ce76
Amanda Krumenauer <krumenaueramy5@gmail.com>
2026-08-17 13:51:52 -0300

    novo home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import './Home.css';
-import tilapia from '../assets/tilapia.png'; 
+import tilapia from '../assets/tilapia.png';
 
 const Home = () => {
   return (
     <section id="home" className="home">
       <div className="home-left">
-        <h1 className="title">Nossos peixes, seu sorriso. Qualidade incomparável!</h1>
-        <p className="subtitle">Eleita a melhor Tilápia fresca da região!</p>
+        <span className="home-tag">PEIXARIA & EMPÓRIO DO VALE</span>
+
+        <h1 className="title">
+          Nossos peixes,
+          <br />
+          seu sorriso.
+        </h1>
+
+        <p className="highlight">Qualidade incomparável!</p>
+
+        <p className="subtitle">
+          Eleita a melhor Tilápia fresca da região!
+        </p>
+
         <a
-          href="https://wa.me/5541991064083" 
+          href="https://wa.me/5541991064083"
           target="_blank"
           rel="noopener noreferrer"
           className="whatsapp-button"
@@ -17,8 +29,13 @@ const Home = () => {
           Faça seu pedido
         </a>
       </div>
+
       <div className="home-right">
-        <img src={tilapia} alt="Tilápia Fresca" className="tilapia-img" />
+        <img
+          src={tilapia}
+          alt="Tilápia fresca"
+          className="tilapia-img"
+        />
       </div>
     </section>
   );
